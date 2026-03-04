7953dbb0b7ce55bcce8033a3b03483c26d281ef9
gabriella-guimaraes <gabigara13@gmail.com>
2026-03-04 18:05:49 -0300

    feat: implementa cálculo de média e manipulação de String

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
 void main() {
-    IO.println("Hello and welcome!");
+    IO.println("Esse é o Screen Match.... o Movie Browser só que em Java");
+
+    int anoLancamento = 2004;
+    String nomeFilme = "Filme: Star Wars III: A Vingança dos Sith";
+    boolean incluidoNoPlano = true;
+    double notaDoFilme = 7.6;
+
+    System.out.println("Filme: " + nomeFilme + ". Lançado em: " + anoLancamento + ". Nota média: " + notaDoFilme);
+    System.out.println("Disponível no seu plano: " + incluidoNoPlano);
+
+    double notaPublico = 9.8;
+    double notaCritica = 7.2;
+    double notaTimeSreenMatch = 9.9;
+
+    double media = (notaPublico + notaCritica + notaTimeSreenMatch) / 3;
+
+    System.out.println(media);
+
+    String sinopse = "Um jedi teve um pesadelo e então explodiu a galáxia";
+    System.out.println(sinopse);
+
 }

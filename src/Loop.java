689d3c356eb2bb49fd821bb6a54fdfc9dcb04f3f
gabriella-guimaraes <gabigara13@gmail.com>
2026-03-10 17:27:47 -0300

    feat: implementa loop for para calcular média de avaliações
    - cria classe Loop
    - utiliza estrutura de repetição for
    - lê três avaliações do usuário com Scanner
    - acumula valores usando operador +=
    - calcula e exibe média das avaliações

---
+++ src/Loop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Loop {
+    static void main() {
+        Scanner leitura = new Scanner(System.in); // objeto da classe Scanner que observa entradas do teclado
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("De 0 a 10 qual nota você dá para o filme?");
+            nota = leitura.nextDouble();
+            mediaAvaliacao += + nota; // incrementa e adiciona a nota
+
+        }
+
+        System.out.println("Média de avaliações " + mediaAvaliacao/3);
+
+    }
+}

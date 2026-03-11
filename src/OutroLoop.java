63fa8448b8f308d2a5c8dd445b5f847ba8dea54b
gabriella-guimaraes <gabigara13@gmail.com>
2026-03-10 22:05:30 -0300

    feat: adiciona classe OutroLoop utilizando while para coletar avaliações até o usuário encerrar

---
+++ src/OutroLoop.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class OutroLoop {
+    static void main() {
+        Scanner leitura = new Scanner(System.in); // objeto da classe Scanner que observa entradas do teclado
+        double mediaAvaliacao = 0;
+        double nota = 0;
+        int totalNotas = 0;
+
+        while (nota != -1) {
+            System.out.println("Digite a sua avaliação para o filme ou -1 para encerrar ");
+            nota = leitura.nextDouble();
+
+            if(nota != -1) {
+                mediaAvaliacao += + nota; // incrementa e adiciona a nota
+                totalNotas++;
+            }
+
+
+        }
+
+        System.out.println("Média de avaliações " + mediaAvaliacao/totalNotas);
+    }
+}

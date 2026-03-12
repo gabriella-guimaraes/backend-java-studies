9be30451cba646c272d663b7d1a17da3ed1faba2
gabriella-guimaraes <gabigara13@gmail.com>
2026-03-12 18:04:55 -0300

    feat: cria classe Filme com atributos básicos do domínio

---
+++ src/Filme.java
@@ -0,0 +1,11 @@
+public class Filme {
+    //Abstração
+    String nome;
+    int anoLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+
+}
